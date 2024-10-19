4c1321769bd5fd032a853ddbc501194a9dffd7ba
alanj268 <alanjose@andrew.cmu.edu>
2024-10-19 16:20:00 -0400

    Stylized website

--- flask_app/app.py
+++ flask_app/app.py
@@ -57,14 +57,14 @@ def calculate_parameters():
 
     delta_l_calc = lambda F, a: ((-(-10) + math.sqrt((-10)**2 - 4 * 7 * (10 - F * a))) / (2 * 7), (-(-10) - math.sqrt((-10)**2 - 4 * 7 * (10 - F * a))) / (2 * 7))
     a = length / 2
-    F = passive_force + active_force / 2
+    F = ((9.8 * passive_force * 0.001) + (9.8 * active_force* 0.001)) / 2
 
     delta_l = delta_l_calc(F, a)
     c = (-2 * a * np.cos(angle) + np.sqrt((2 * a * np.cos(angle))**2 - 4 * (1) * (a**2 - length**2))) / 2
     calculated_length = np.round(a + c - delta_l, 2)
-    calculated_angle = np.round(np.arccos((calculated_length**2 - a**2 - c**2) / (2 * a * c)))
+    calculated_angle = angle
 
-    return render_template('parameterization.html', calculated_length=calculated_length[0], calculated_angle=calculated_angle[0])
+    return render_template('parameterization.html', calculated_length=calculated_length[0], calculated_angle=calculated_angle)
 
 
 if __name__ == '__main__':
